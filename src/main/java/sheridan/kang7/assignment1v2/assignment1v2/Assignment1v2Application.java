961300dba6e9549e6e8f286229637ea3f0e6e39f
Gursimran Kaur Kang <kang7@sheridancollege.ca>
2021-06-04 14:36:32 -0400

    created an empty project

---
+++ src/main/java/sheridan/kang7/assignment1v2/assignment1v2/Assignment1v2Application.java
@@ -0,0 +1,13 @@
+package sheridan.kang7.assignment1v2.assignment1v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1v2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment1v2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/kang7/assignment1v2/assignment1v2/Assignment1v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.kang7.assignment1v2.assignment1v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1v2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
