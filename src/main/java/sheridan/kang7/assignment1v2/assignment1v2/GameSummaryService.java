91dee85cd2b2ecf7caee34b0392cfc7fe8043a3b
Gursimran Kaur Kang <kang7@sheridancollege.ca>
2021-06-05 12:51:30 -0400

    create two classes

---
+++ src/main/java/sheridan/kang7/assignment1v2/assignment1v2/GameSummaryService.java
@@ -0,0 +1,7 @@
+package sheridan.kang7.assignment1v2.assignment1v2;
+
+@RestController
+public class GameSummaryService {
+    @Autowired
+    private GameSummaryRepository gameSummaryRepository;
+}
---
+++ src/main/java/sheridan/kang7/assignment1v2/assignment1v2/Gesture.java
@@ -0,0 +1,6 @@
+package sheridan.kang7.assignment1v2.assignment1v2;
+
+public enum Gesture {
+    PAPER, ROCK, SCISSORS;
+
+}
